8407c8bd73c6e7c7bcb648bb3ef2bf19c83855af
Vasu-Gupta-kpt <105506600+Vasu-Gupta-kpt@users.noreply.github.com>
2024-09-18 15:21:25 +0530

    Update CalculatorTest.java
    0 coverage

--- src/test/java/ut/CalculatorTest.java
+++ src/test/java/ut/CalculatorTest.java
@@ -9,38 +9,38 @@ import com.example.Calculator;
 
 public class CalculatorTest {
 
-	static Calculator cl;
+	// static Calculator cl;
 
-	@BeforeClass
-	public static void setUp() {
-		cl = new Calculator();
-	}
+	// @BeforeClass
+	// public static void setUp() {
+	// 	cl = new Calculator();
+	// }
 
-	@AfterClass
-	public static void tearDown() {
-		System.gc();
-	}
+	// @AfterClass
+	// public static void tearDown() {
+	// 	System.gc();
+	// }
 
-	@Test
-	public void testAdd() {
+	// @Test
+	// public void testAdd() {
 
-		assertTrue(cl.doAdd(1, 2) == 3);
-	}
+	// 	assertTrue(cl.doAdd(1, 2) == 3);
+	// }
 
-	@Test
-	public void testSub() {
-		assertTrue(cl.doSub(1, 1) == 0);
-	}
+	// @Test
+	// public void testSub() {
+	// 	assertTrue(cl.doSub(1, 1) == 0);
+	// }
 
-	@Test
-	public void testMul() {
-		assertTrue(cl.doMul(9, 2) == 18);
-	}
+	// @Test
+	// public void testMul() {
+	// 	assertTrue(cl.doMul(9, 2) == 18);
+	// }
 
-	@Test
-	public void testDiv() {
-		assertTrue(cl.doDiv(8, 2) == 4);
-	}
+	// @Test
+	// public void testDiv() {
+	// 	assertTrue(cl.doDiv(8, 2) == 4);
+	// }
 	
 
 }
